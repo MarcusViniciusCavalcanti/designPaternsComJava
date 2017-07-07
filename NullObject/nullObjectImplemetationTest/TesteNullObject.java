11a0ebc4979ebdf326158726848f191cb214d64d
Marcus Cavalcanti <vincius.cavalcanti16@gmail.com>
2017-07-06 22:13:12 -0300

    null object

--- NullObject/nullObjectImplemetationTest/TesteNullObject.java
+++ NullObject/nullObjectImplemetationTest/TesteNullObject.java
@@ -5,26 +5,29 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import nullObjectImplemetation.Curse;
 import nullObjectImplemetation.NullCurse;
 import nullObjectImplemetation.Pessoa;
 
 public class TesteNullObject {
 
 	public static void main(String[] args) {
-		Pessoa pessoaWithCurse = new Pessoa("com curso", 20, new NullCurse("Curso teste", LocalTime.of(14, 00)));
-		Pessoa pessoaNotCurse = new Pessoa("com curso", 20, new NullCurse());
+		Curse curse = new NullCurse("Curso teste", LocalTime.of(14, 00));
+		Pessoa pessoaWithCurse = new Pessoa("com curso", 20, curse);
 		
+		Curse curse2 = new NullCurse();
+		Pessoa pessoaNotCurse = new Pessoa("com curso", 20, curse2);
 		
 		List<Pessoa> pessoas = new ArrayList<>();
 		pessoas.add(pessoaWithCurse);
 		pessoas.add(pessoaNotCurse);
-		
-		
+			
 		for (Pessoa pessoa : pessoas) {
-			System.out.println(pessoa.getName());
-			System.out.println(pessoa.getAge());
-			System.out.println(pessoa.getCurse().getName() + " horário: " + pessoa.getCurse().curseTime());
-		}
+			System.out.println(
+				"Nome: " + pessoa.getName() +
+				"Idade: " + pessoa.getAge() +
+				"Curso cadastrado: " + pessoa.getCurse().getName() + " horário: " + pessoa.getCurse().curseTime());
+		}	
 		
 
 	}
